4935825407e2f373c93df643250ccda43e74f76a
BuenoMBP <marcos_vbp@outlook.com>
2026-03-06 22:50:04 -0300

    Update Card Component

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,20 @@ import './App.css'
 function App() {
 
   return (
-    <body className="main">
-      <h1>Teste</h1>
+    <body className='main'>
+      <section className='content'>
+        <h1>Central dos crias</h1>
+        <div className='intro'>
+          <h1>Bem-vindo ao meu site!</h1>
+        </div>
+        <div className='container'>
+          <Card img={image} name='Card 1' />
+          <Card img={image} name='Card 2' />
+          <Card img={image} name='Card 3' />
+          <Card img={image} name='Card 4' />
+        </div>
+      </section>
       {/* <Button text="qql coisa" action={() => console.log('Button clicked!')} /> */}
-      <Card img={image} />
     </body>
   )
 }
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,13 +1,15 @@
 import styles from './style.module.css'
 
 type CardProps = {
-    img: string
+    img: string;
+    name: string;
 }
 
 function Card(props: CardProps) {
     return (
         <button className={styles.main}>
             <img src={props.img} alt="Card image" className={styles.image} />
+            <h2>{props.name}</h2>
         </button>
     )
 }
